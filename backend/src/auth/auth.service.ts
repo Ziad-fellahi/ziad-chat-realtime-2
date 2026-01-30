d56cfa4daf02d53719a95d0a57356aae921239bd
Ziad Fellahi <ziad.fellahi@outlook.fr>
2026-01-30 15:08:48 +0100

    Add auth dependencies

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from './user.model';
+import { User, UserDocument } from './user.schema';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 
